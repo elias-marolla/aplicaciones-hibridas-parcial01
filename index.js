d917baeaec55d521539e804c5fa1a07ecf8b69d0
eliasmarolla <elias.marolla@gmail.com>
2025-09-12 17:12:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import routerApi from "./routes/index.js"
+import routerApi from "./routes/index.js";
+
 dotenv.config();
-const port = process.env.PORT;
+const PORT = process.env.PORT;
+const URI_DB = process.env.URI_DB;
+const app = express();
+
+mongoose.connect(URI_DB);
+const db = mongoose.connection;
+db.on('error', ()=>{console.error('no se genero la conexion con la base de datos.')});
+db.once('open', () => {console.info('se establecio la conexion con la base de datos.')});
