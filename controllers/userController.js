769bf5a6ccc4956aa3fd3a8a2cf01d8634e3eb69
eliasmarolla <elias.marolla@gmail.com>
2025-09-22 23:49:37 -0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,9 @@ import UserModel from "../models/userModel.js";
 const getUsers = async (req , res ) => {
     try {
         const users = await UserModel.find();
-        res.status(200).json({message: 'Usuarios encontrados.', data: users });
+        res.status(200).json({message: "Usuarios encontrados.", data: users });
     } catch (error) {
-        res.status(500).json({message: 'no se pudo obtener usuarios.'})
+        res.status(500).json({message: "no se pudo obtener usuarios."})
     }
 }
 
@@ -15,10 +15,37 @@ const postUser = async (req , res ) => {
         const {name, email, password} = req.body;
         const user = new UserModel({name, email, password});
         user.save();
-        res.status(201).json({message:'Se creo un nuevo usuario'})
+        res.status(201).json({message:"Se creo un nuevo usuario"})
     } catch (error) {
-        res.status(500).json({message:'No se pudo generar el nuevo usuario.'})
+        res.status(500).json({message:"No se pudo generar el nuevo usuario."})
     }
 }
 
-export {getUsers, postUser}
+const getUserById = async (req , res ) => {
+    try{
+        const user = await UserModel.findById(req.params.id);
+        if (!user) {
+            res.status(404).json({message: "Usuario no encontrado"})
+        }
+        res.status(200).json(user);
+    } catch (error){
+        console.error (error);
+        res.status(500).json({message: "Error al obtener usuario"});
+    }
+}
+
+const deleteUser = async (req , res ) => {
+    try {
+        const { id } = req.params;
+        const deleted = await CategoriaModel.findByIdAndDelete(id);
+        if (!deleted) {
+            return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+        res.status(200).json({ message: "Usuario eliminado" });
+    } catch (error) {
+        res.status(500).json({ message: "Error al eliminar categoría" });
+    }
+}
+
+
+export {getUsers, postUser, getUserById, deleteUser}
