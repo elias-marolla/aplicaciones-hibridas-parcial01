255fdd743cd81a6fb264087d8632f7948e6d9024
Diego Asensio <dieasensio@gmail.com>
2025-09-24 20:52:48 -0300

    Endpoints funcionales en el index

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Rutas
 import routerApi from "./routes/index.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3030;
 const URI_DB = process.env.URI_DB;
 
-
-mongoose.connect(URI_DB);
-const db = mongoose.connection;
-db.on('error', ()=>{console.error('no se genero la conexion con la base de datos.')});
-db.once('open', () => {console.info('se establecio la conexion con la base de datos.')});
+// Conexión a la base de datos
+mongoose
+  .connect(URI_DB)
+  .then(() => console.log("Conexión a MongoDB establecida"))
+  .catch((err) => console.error("Error al conectar con MongoDB:", err));
 
 const app = express();
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("/", express.static(path.join(__dirname, "views")));
+
+// Endpoints de la API
 routerApi(app);
 
-app.listen (PORT, ()=>{
-    console.log('API levantada en el puerto:' + PORT);
-})
+// Levantamos el servidor
+app.listen(PORT, () => {
+  console.log(`API levantada en http://localhost:${PORT}`);
+});
