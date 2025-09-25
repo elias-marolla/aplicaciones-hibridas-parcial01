47da2b50d37e09759b9bad6158e377df4c72b4f1
eliasmarolla <elias.marolla@gmail.com>
2025-09-25 11:21:59 -0300

    correccion

--- models/userModel.js
+++ models/userModel.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const Scm = new Schema({
-    email: String,
-    name: String,
-    password: String
+    email: { type: String, required: true, unique: true},
+    name: { type: String },
+    password: { type: String }
 });
 
 const model = mongoose.model('User',Scm);
